fbdb0cb27db81eb4b30183d5e550b8d351849eea
Sebastian <sebasmontero1503@udistrital.edu.co>
2025-10-18 21:48:42 -0500

    Creacion del proyecto

---
+++ app/src/main/java/com/example/parcial1/data/ParadaPits.kt
@@ -0,0 +1,19 @@
+package com.example.parcial1.data
+
+data class ParadaPits (
+
+    var piloto: String,
+    var escuderia:String,
+    var tiempo: Double,
+    var neumatico: String,
+    var numNeumaticos: Int,
+    var estado: String,
+    var motivo: String?,
+    var mecanico: String,
+    var fecha:String,
+    var hora: String
+
+
+    )
+
+
